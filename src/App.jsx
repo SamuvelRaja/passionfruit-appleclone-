2dd54b0f27bfaf2274700dd16f561dbb29c93e3d
SamuvelRaja <josephsamuvel001@gmail.com>
2024-06-17 21:39:00 +0530

    added tailwind, gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+useGSAP(()=>{
+  gsap.to(".go",{
+    y:500,
+    repeat:-1,
+    yoyoEase:true,
+    ease:"power1.inOut"
+  })
+},[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <h1 className="text-3xl font-bold text-black underline go">
+      Hello world!
+    </h1>
   )
 }
 
