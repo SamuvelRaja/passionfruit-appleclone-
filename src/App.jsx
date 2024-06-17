321da0d5f11cdaa0722a63224430e58c0d8b4a37
SamuvelRaja <josephsamuvel001@gmail.com>
2024-06-17 21:52:52 +0530

    added assets. and starter files

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 
 function App() {
-useGSAP(()=>{
-  gsap.to(".go",{
-    y:500,
-    repeat:-1,
-    yoyoEase:true,
-    ease:"power1.inOut"
-  })
-},[])
+
+
   return (
-   <h1 className="text-3xl font-bold text-black underline go">
-      Hello world!
-    </h1>
+  <>
+    
+  </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const header = () => {
+  return (
+    <header className="flex justify-between">
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <nav><a href=""></a></nav>
+        </div>
+    </header>
+  )
+}
+
+export default header
