560146f083925b48ce8129541c8952c4a410dda5
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-02 16:14:38 +0530

    added animation

--- src/components/3dphone.jsx
+++ src/components/3dphone.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import { OrbitControls, View, PerspectiveCamera } from '@react-three/drei'
 import  Model  from './object'
 import Lights from './lights'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 
 
@@ -18,15 +20,23 @@ const Iphone = ({}) => {
 
       const [selectedColor, setSelectedColor] = useState(colors[0]); // Initialize with first color
       const [isPro, setIsPro] = useState(false); // Pro model state
-
+      console.log(selectedColor,"ss")
       const handleColorChange = (color) => {
         setSelectedColor(color);
+        
       };
 
       const handleProToggle = () => {
         setIsPro(prevState => !prevState);
       };
 
+      useGSAP(()=>{
+        gsap.to(".tg-bar",{
+          marginLeft:isPro?'50%':'0%',
+          duration:0.5,
+          ease:"expo.out"
+        })
+    },[isPro])
   return (
         <>
           <Canvas shadows dpr={[1, 1.5]} gl={{ antialias: false }} >
@@ -40,21 +50,25 @@ const Iphone = ({}) => {
               {colors.map((color) => (
                 <button
                   key={color.id}
-                  className={`px-3  mx-1 bg-[${color.code}] text-white font-bold rounded-full
-                  ${selectedColor.id === color.id ? 'border-[#fff]' : ''}`}
+                  className={`mx-1 text-white font-bold rounded-full
+                  ${selectedColor.id === color.id ? 'border-2 border-white px-[11px]' : 'px-[13px]'}`}
                   style={{ backgroundColor: color.code }}
                   onClick={() => handleColorChange(color)}
                 />
               ))}
             </div>
-            <div className="pro-toggle w-full max-w-[290px] rounded-full backdrop-blur bg-gray-300 h-[50px]  p-[8px]   flex ">
+            <div className="pro-toggle w-full max-w-[290px] rounded-full backdrop-blur bg-gray-300 h-[50px]   flex ">
               <div className='flex w-full '>
-                <div className='rounded-full bg-[#fff]  w-[48%]'>
+                <div className='rounded-full bg-[#fff] border-2 border-[#0000bb] shadow-white tg-bar w-[48%]'>
                 </div>
               </div>
-              <div className="tgbtn h-[34px] items-center flex absolute bg-transparent margin text-[#000]">
-                <p className="bg-transparent px-3">15.54 CM (6.1")</p>
-                <p className="bg-transparent">17.00CM (6.7")</p>
+              <div className="tgbtn h-full gap-2 items-center flex absolute bg-transparent text-[17px] font-normal ">
+                <p className={`bg-transparent px-2 text-center ${isPro?"text-[#fff]":"text-[#000]"}`}
+                  onClick={() => handleProToggle()}
+                >15.54 CM (6.1")</p>
+                <p className={`bg-transparent px-3 text-center ${isPro?"text-[#000]":"text-[#fff]"}`}
+                  onClick={() => handleProToggle()}
+                  >17.00CM (6.7")</p>
               </div>
             </div>
             
